7ba557795a46fe6547b6fd5eeae4da3943704aef
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-12-22 00:53:19 +0500

    стили цвета

---
+++ my-react/src/components/tariff.jsx
@@ -0,0 +1,29 @@
+import './tariff.css'
+
+function Tarifs(props) {
+    const {item} = props;
+    
+    let color = 'black'
+
+    if (item.id === 0) {
+      color = '#87CEEB'
+    } else if (item.id === 1 ) {
+        color = '#2E8B57'
+    } else if (item.id === 2 ) {
+        color = '#FF0000'
+    } else if (item.id === 3 ) {
+        color = '#000000'
+    } 
+   
+
+    return (
+        <div className="card">
+         <div className="card_title" style={{backgroundColor: `${color}` }}>{ item.name }</div>
+         <div className="card_item1" style={{backgroundColor: `${color}` }} >{ item.price }</div>
+         <div className="card_item2">{ item.speed }</div>
+         <div className="card_item3" >{ item.description }</div>
+         </div>
+    )
+}
+
+export default Tarifs;
